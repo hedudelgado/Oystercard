2c5b06451ed0e02d20b9ec890ce0c1c16b020c30
hedudelgado <hedu.delgado@gmail.com>
2016-01-25 16:51:50 +0000

    top up function on

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,15 @@
-describe Oystercard do 
+require 'oystercard'
+
+describe Oystercard do
+	describe 'balance' do 
+    	it 'should return a balance' do
+    	expect(subject.balance).to eq 0
+		end
+    end
+    describe '#top_up' do 
+		it 'should be able to add money and increase the balance' do 
+		subject.top_up(5)
+		expect(subject.balance).to eq 5	
+		end
+	end
 end
