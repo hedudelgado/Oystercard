390b145e9d83179e4faffd4841587ca4d8e3d108
hedudelgado <hedu.delgado@gmail.com>
2016-01-25 17:30:14 +0000

    deduct test and method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,17 @@
 class Oystercard
 	attr_reader :balance
+	MAX_LIMIT = 90
 
 	def initialize
 		@balance = 0
 	end
 
 	def top_up(money)
+		raise "the amount exceed #{MAX_LIMIT}" if (@balance + money) >= MAX_LIMIT
 		@balance += money
+	end
+
+	def deduct(money)
+		@balance -= money
 	end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,5 +11,16 @@ describe Oystercard do
 		subject.top_up(5)
 		expect(subject.balance).to eq 5	
 		end
+		it 'raise an error when the top up is over the limit' do
+		mes = "the amount exceed #{Oystercard::MAX_LIMIT}"
+		expect{subject.top_up(Oystercard::MAX_LIMIT)}.to raise_error mes
+		end
 	end
+	describe '#deduct' do 
+		it 'reduces money from the oyster' do 
+		subject.top_up(5)
+		subject.deduct(5)
+		expect(subject.balance).to eq 0
+        end
+    end
 end
