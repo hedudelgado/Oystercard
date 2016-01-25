fdb67733d9277aad5bc91d49efee3d7b504f3453
hedudelgado <hedu.delgado@gmail.com>
2016-01-25 19:01:05 +0000

    charge and minimun required

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
 	attr_reader :balance
 	MAX_LIMIT = 90
+	MIN = 1
 
 	def initialize
 		@balance = 0
@@ -12,21 +13,24 @@ class Oystercard
 		@balance += money
 	end
 
-	def deduct(money)
-		@balance -= money
-	end
-
 	def journey?
 		@journey
 	end
 
 	def touch_in
+		raise 'not enough founds, need to top up' if @balance < MIN
 		@journey = true
 	end
 
 	def touch_out
+		deduct(MIN)
 		@journey = false
 	end
 
+private
+
+	def deduct(money)
+		@balance -= money
+	end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -16,30 +16,36 @@ describe Oystercard do
 		expect{subject.top_up(Oystercard::MAX_LIMIT)}.to raise_error mes
 		end
 	end
-	describe '#deduct' do 
-		it 'reduces money from the oyster' do 
-		subject.top_up(5)
-		subject.deduct(5)
-		expect(subject.balance).to eq 0
-        end
-    end
 
     describe'#journey?' do
         it 'should return false at first as the oyster' do
         expect(subject.journey?).to eq false
         end
     end
+
     describe '#touch_in' do 
-        it 'should change the journey status of the oyster card to true' do 
+        it 'should change the journey status of the oyster card to true' do
+        subject.top_up(5) 
         subject.touch_in
         expect(subject.journey?).to eq true
         end
+          it 'raise an error when the oyster has less than 1 pound founds' do 
+        expect{subject.touch_in}.to raise_error 'not enough founds, need to top up'
+        end
     end
+
       describe '#touch_out' do 
-        it 'should change the journey status of the oyster card to false' do 
+        it 'should change the journey status of the oyster card to false' do
+        subject.top_up(5)  
         subject.touch_in
         subject.touch_out
         expect(subject.journey?).to eq false
         end
+          it 'should change the journey status of the oyster card to false' do
+        subject.top_up(5)  
+        subject.touch_in
+        expect {subject.touch_out}.to change{subject.balance}.by(-Oystercard::MIN)
+        end
     end
+   
 end
