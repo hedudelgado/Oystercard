c45b073b505a1d87e860f859df254b62e64b1bff
Mark Hill <futonfutonda@gmail.com>
2016-01-26 12:47:13 +0000

    added touch-in, touch-out, and in-journey

--- lib/make_oystercard.rb
+++ lib/make_oystercard.rb
@@ -2,11 +2,12 @@ class OysterCard
 
   	MAX_BALANCE = 90
 	attr_reader :balance
-  
+
 	def initialize
     	@balance = 0
+      @in_journey = false
   	end
-  
+
   	def top_up(value)
     	raise "Amount exceeds the limit: #{MAX_BALANCE}" if @balance +value > MAX_BALANCE
     	@balance += value
@@ -17,4 +18,15 @@ class OysterCard
   		return value
   	end
 
+    def touch_in
+      @in_journey = true
+    end
+
+    def touch_out
+      @in_journey = false
+    end
+
+    def in_journey?
+      @in_journey
+    end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -18,22 +18,29 @@ describe "#top_up"do
   expect{ subject.top_up(OysterCard::MAX_BALANCE + 1) }.to raise_error "Amount exceeds the limit: #{OysterCard::MAX_BALANCE}"
   end
 end
-  describe '#deduct' do 
-  it 'demostrates tha the money is actually deducted' do 
+  describe '#deduct' do
+  it 'demostrates tha the money is actually deducted' do
   subject.top_up(10)
   expect{ subject.deduct 6}. to change{ subject.balance}.by -6
   end
-  it 'return the amount deducted' do 
+  it 'return the amount deducted' do
   subject.top_up(10)
   expect(subject.deduct 6).to eq 6
   end
   end
+  it { is_expected.to respond_to(:in_journey?) }
+
+  describe '#touch_in'do
+    it "allows you to touch in"do
+      subject.touch_in
+      expect(subject.in_journey?).to eq true
+    end
+  end
+  describe '#touch_out'do
+    it 'allows you to touch out'do
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey?).to eq false
+    end
+  end
 end
-
-
-
-
-
-
-
-
