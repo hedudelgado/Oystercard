33286d4d711809927080cbb5b85937c2bdc7802b
hedudelgado <hedu.delgado@gmail.com>
2016-01-28 23:45:28 +0000

    #outstanding_charges ready! and refactorize

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,33 +1,59 @@
 require_relative 'oystercard.rb'
+require_relative 'Journey'
+
 class JourneyLog
 
 	attr_reader :current
 	def initialize
 		@journey = []
 		@current = {}
+		@completetracker = nil
 	end
 
 	def start_journey(station)
-		# outstanding
 		@current.clear
-		@current[:entry] = station 
-		@journey.push("entry: #{station}")
+		if 	complete_in? then save_in_good(station) else save_in_penalty(station) end
 	end
-
+	
 	def journeys
 		history
 	end
 
 	def exit_journey(station)
-		@current[:exit]  = station
-		@journey.push("exit: #{station}")
+		if  complete_exit? then save_exit_good(station) else save_exit_penalty(station) end
 	end
 
-	# def outstanding
-	#  	if @journey.last == :entry then @current[:exit]= "incomplete journey, fare #{Journey::PENALTY_FARE} pounds."end	
-	# end
 
 	private
+
+	def save_exit_penalty(station)
+		outstanding_charges
+		@journey.push("exit: #{station}")
+		@completetracker = nil
+	end
+
+
+	def save_exit_good(station)
+		@current[:exit]  = station
+		@journey.push("exit: #{station}")
+		@completetracker = nil
+	end
+
+	def save_in_penalty(station)
+		outstanding_charges
+		@journey.push("entry: #{station}")
+		@completetracker = true
+	end
+
+	def outstanding_charges
+		@journey.push("incomplete journey, #{Journey::PENALTY_FARE} pounds")
+	end
+
+	def save_in_good(station)
+		@current[:entry] = station 
+		@journey.push("entry: #{station}")
+		@completetracker = true
+	end
 
 	def current_journey #a private method #current_journey should return an incomplete 
 		#journey or create a new journey
@@ -36,6 +62,13 @@ class JourneyLog
 	def history
 		@journey
 	end
+	def complete_exit?
+		@completetracker == true
+	end
+	def complete_in?
+		@completetracker == nil
+	end
+
 end
 
 
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -8,13 +8,23 @@ describe JourneyLog do
       		subject.start_journey(station)
       		expect(subject.current).to include(:entry => station)
     	end
+      it 'will check if the journey is completed' do 
+          subject.start_journey(station)
+          subject.start_journey(station)
+          expect(subject.journeys).to eq ["entry: #{station}","incomplete journey, #{Journey::PENALTY_FARE} pounds", "entry: #{station}"]
+      end
+
     end
-    describe '#exit_journey' do 	
-    	it 'will memorize the start and finish' do
+  describe '#exit_journey' do 	
+    	it ' should add a new exit station to the current journey' do
       		subject.start_journey(station)
       		subject.exit_journey(station)
       		expect(subject.current).to include(:entry => station, :exit => station)
       	end
+        it ' should add a incomplete trip' do
+          subject.exit_journey(station)
+          expect(subject.journeys).to eq ["incomplete journey, #{Journey::PENALTY_FARE} pounds", "exit: #{station}"]
+        end
     end
 
     describe '#journeys' do 
@@ -26,6 +36,7 @@ describe JourneyLog do
       		expect(subject.journeys).to eq ["entry: #{station}","exit: #{station}","entry: #{station}","exit: #{station}"]
       	end  
     end
+
     # describe '#outstanding' do 
     # 	it 'close an incomplete journey and return its fare' do 
     # 		subject.start_journey(station)
