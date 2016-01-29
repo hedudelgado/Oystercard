63a9b706af8638481044411f123fcef714acb462
hedudelgado <hedu.delgado@gmail.com>
2016-01-29 13:46:50 +0000

    closing trips

--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -29,16 +29,24 @@ describe JourneyLog do
 
     describe '#outstanding_charges' do
       it 'should close an incomplete journey and return its fare' do 
-        e = [{entry_station: station, trip: nil}]
+        a = [{entry_station: station, trip: nil}]
         subject.start_journey(station)
         subject.start_journey(station)
         expect(subject.journeys).to eq e 
       end
       it 'should close an incomplete 2nd case' do 
-        a = [{entry_station: station, exit_station: station},{entry_station: station, trip: nil}]
+        a = [{entry_station: station, exit_station: station},{trip: nil, exit_station: station}]
         subject.start_journey(station)
         subject.exit_station(station)
         subject.exit_station(station)
+        expect(subject.journeys).to eq a
+      end
+       it 'should close an incomplete 2nd case' do 
+        a = [{entry_station: station, exit_station: station},{entry_station: station, trip: nil }]
+        subject.start_journey(station)
+        subject.exit_station(station)
+        subject.start_journey(station)
+        subject.start_journey(station)
         expect(subject.journeys).to eq a
       end
     end
